427c16527020c4d44e7ceef9cacce35dff4d289f
Julie Engel <jeng@Julies-MacBook-Air.local>
2026-03-11 19:10:21 +0100

    add remap for nohlsearch

--- lua/jeng/remap.lua
+++ lua/jeng/remap.lua
@@ -79,3 +79,5 @@ vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 vim.keymap.set({ "n", "v" }, "<leader>d", "\"_d")
 vim.keymap.set("i", "<C-c>", "<Esc>")
+
+vim.keymap.set("n", "//", vim.cmd.nohlsearch)
